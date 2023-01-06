b48bdf76ba66325b76ca932ea1b394a830eca9e7
srao-star <122068270+srao-star@users.noreply.github.com>
2023-01-05 18:48:23 -0800

    Update streamlit_app.py
    added the items

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,9 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
+
+streamlit.text('Craneberry, Muffins')
+
+streamlit.text('coffee')
+
+streamlit.text('yogurt')
